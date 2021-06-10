b95a746336ad8f5323053f3d6501ebb930a1ce97
Christopher Campbell <campbefs@gmail.com>
2021-06-10 16:05:37 -0700

    test

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+}); 
+
+//test
 
 // Use this to log mongo queries being executed
 mongoose.set('debug', true);
